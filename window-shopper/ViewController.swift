04412ecea8bd9ddd99bb88227cba70ed7d655fdc
MarwaZabara <marwa_zabara@yahoo.com>
2018-09-06 19:41:00 +0200

    initial commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Marwa Zabara on 9/6/18.
+//  Copyright © 2018 Marwa Zabara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
